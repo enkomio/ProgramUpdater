0cb16dcc6327906867a622e4fb37b6e833dda8a3
Antonio Parata <aparata@gmail.com>
2019-07-25 17:06:15 +0200

    Key generator project created

---
+++ Src/KeysGenerator/Program.fs
@@ -0,0 +1,25 @@
+﻿namespace KeysGenerator
+
+open System
+open System.Security.Cryptography
+
+module Program =
+    [<EntryPoint>]
+    let main argv =
+        use key =
+            (new ECDiffieHellmanCng(
+                KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash,
+                HashAlgorithm = CngAlgorithm.Sha256
+            )).Key
+
+        let privateBytes = key.Export(CngKeyBlobFormat.EccPrivateBlob)
+        let publicBytes = key.Export(CngKeyBlobFormat.EccPublicBlob)
+        
+        // data to print
+        Console.WriteLine("-=[ Secret keys ]=-")
+        Console.WriteLine()
+        Console.WriteLine("Private key: {0}", Convert.ToBase64String(privateBytes))
+        Console.WriteLine()
+        Console.WriteLine("Public key: {0}", Convert.ToBase64String(publicBytes))
+        
+        0
