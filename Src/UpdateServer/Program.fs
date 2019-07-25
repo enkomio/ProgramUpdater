117ec2157dd95e5fa53ced375d8cbbf2a01ac471
Antonio Parata <aparata@gmail.com>
2019-07-25 18:07:09 +0200

    Server code of the updater

---
+++ Src/UpdateServer/Program.fs
@@ -0,0 +1,25 @@
+﻿namespace UpdateServer
+
+open System
+open System.IO
+open ES.Fslog
+open System.Reflection
+open ES.Fslog.Loggers
+open ES.Fslog.TextFormatters
+open ES.Update.Backend
+
+module Program =
+    let private configureLogProvider(isVerbose: Boolean) =
+        let path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+        let logProvider = new LogProvider()    
+        let logLevel = if isVerbose then LogLevel.Verbose else LogLevel.Informational
+        logProvider.AddLogger(new ConsoleLogger(logLevel, new ConsoleLogFormatter()))
+        logProvider.AddLogger(new FileLogger(logLevel, Path.Combine(path, "updater-server.log")))
+        logProvider :> ILogProvider
+
+    [<EntryPoint>]
+    let main argv = 
+        let logProvider = configureLogProvider(false)
+        let server = new Server("http://127.0.0.1", logProvider)
+        server.Start()
+        0
