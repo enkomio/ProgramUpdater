ee3ca7509913b1a12c66fce9067f2db29f4b850b
Antonio Parata <aparata@gmail.com>
2019-08-01 12:46:49 +0200

    Added settings:

---
+++ Src/UpdateServer/Settings.fs
@@ -0,0 +1,18 @@
+﻿namespace UpdateServer
+
+open System
+open System.Collections.Generic
+open System.IO
+open Newtonsoft.Json
+open System.Reflection
+
+type Settings() =
+    member val WorkspaceDirectory = String.Empty with get, set
+    member val BindingAddress = String.Empty with get, set
+
+    static member Read() =
+        let configFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "configuration.json")
+        if File.Exists(configFile) then
+            (File.ReadAllText >> JsonConvert.DeserializeObject<Settings>)(configFile)
+        else
+            new Settings()
