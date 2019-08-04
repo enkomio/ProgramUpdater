07bf6bb69adf2577e3458e157018e3cfae9d8cf3
Antonio Parata <aparata@gmail.com>
2019-08-04 17:54:01 +0200

    Implemented export key

--- Src/UpdateServer/Program.fs
+++ Src/UpdateServer/Program.fs
@@ -2,7 +2,7 @@
 
 open System
 open System.IO
-open System.Security.Cryptography
+open Argu
 open ES.Fslog
 open System.Reflection
 open ES.Fslog.Loggers
@@ -10,13 +10,46 @@ open ES.Fslog.TextFormatters
 open ES.Update.Backend
 
 module Program =
+    type CLIArguments =
+        | Export_Key of file:String
+        | Import_Key of file:String
+        | Verbose
+    with
+        interface IArgParserTemplate with
+            member s.Usage =
+                match s with
+                | Export_Key _ -> "export the private key to the specified file."
+                | Import_Key _ -> "import the private key from the specified file."
+                | Verbose -> "print verbose log messages."
+
     let private _logger =
         log "UpdateServer"
         |> info "CreateKeys" "Encryption keys not found. Generating them"
         |> info "PublicKey" "Public key: {0}"
         |> info "PrivateKey" "Private key first bytes: {0}"
         |> info "KeysCreated" "Encryption keys created and saved to files. The public key must be distributed togheter with the updater"
+        |> info "KeyExported" "Private key exported to file: {0}"
         |> build
+
+    let private printColor(msg: String, color: ConsoleColor) =
+        Console.ForegroundColor <- color
+        Console.WriteLine(msg)
+        Console.ResetColor() 
+
+    let private printError(errorMsg: String) =
+        printColor(errorMsg, ConsoleColor.Red)
+
+    let private printBanner() =
+        Console.ForegroundColor <- ConsoleColor.Cyan        
+        let banner = "-=[ Version Releaser ]=-"
+        let year = if DateTime.Now.Year = 2019 then "2019" else String.Format("2019-{0}", DateTime.Now.Year)
+        let copy = String.Format("Copyright (c) {0} Enkomio {1}", year, Environment.NewLine)
+        Console.WriteLine(banner)
+        Console.WriteLine(copy)
+        Console.ResetColor()
+        
+    let private printUsage(body: String) =
+        Console.WriteLine(body)
 
     let private configureLogProvider(isVerbose: Boolean) =
         let path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
@@ -24,12 +57,17 @@ module Program =
         let logLevel = if isVerbose then LogLevel.Verbose else LogLevel.Informational
         logProvider.AddLogger(new ConsoleLogger(logLevel, new ConsoleLogFormatter()))
         logProvider.AddLogger(new FileLogger(logLevel, Path.Combine(path, "updater-server.log")))
-        logProvider :> ILogProvider    
+        logProvider :> ILogProvider  
+        
+    let private getKeyFileNames() =
+        let curDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)
+        (
+            Path.Combine(curDir, "private.bin"),
+            Path.Combine(curDir, "public.bin")
+        )
 
     let private createEncryptionKeys(settings: Settings) =
-        let curDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)
-        let privateFile = Path.Combine(curDir, "private.bin")
-        let publicFile = Path.Combine(curDir, "public.bin")
+        let (privateFile, publicFile) = getKeyFileNames()
 
         if not(File.Exists(privateFile)) || not(File.Exists(publicFile)) then
             _logger?CreateKeys()
@@ -47,14 +85,40 @@ module Program =
 
     [<EntryPoint>]
     let main argv = 
-        let logProvider = configureLogProvider(false)
-        logProvider.AddLogSourceToLoggers(_logger)
-        let settings = Settings.Read()
+        printBanner()
+        
+        let parser = ArgumentParser.Create<CLIArguments>()
+        try            
+            let results = parser.Parse(argv)
+            
+            let logProvider = configureLogProvider(results.Contains(<@ Verbose @>))
+            logProvider.AddLogSourceToLoggers(_logger)            
 
-        // no private keys specified, read the content from file
-        if String.IsNullOrWhiteSpace(settings.PrivateKey) then 
-            createEncryptionKeys(settings)
+            if results.IsUsageRequested then
+                printUsage(parser.PrintUsage())
+                0
+            else
+                let settings = Settings.Read()
 
-        let server = new Server(settings.BindingAddress, settings.WorkspaceDirectory, settings.PrivateKey)
-        server.Start()
-        0
+                // no private keys specified, read the content from file
+                if String.IsNullOrWhiteSpace(settings.PrivateKey) then 
+                    createEncryptionKeys(settings)
+
+                if results.Contains(<@ Export_Key @>) then
+                    let fileName = results.GetResult(<@ Export_Key @>)
+                    File.WriteAllText(fileName, settings.PrivateKey)
+                    _logger?KeyExported(fileName)
+                elif results.Contains(<@ Import_Key @>) then
+                    let filename = results.GetResult(<@ Import_Key @>)
+                    ()
+                else
+                    let server = new Server(settings.BindingAddress, settings.WorkspaceDirectory, settings.PrivateKey)
+                    server.Start()       
+                0
+        with 
+            | :? ArguParseException ->
+                printUsage(parser.PrintUsage())   
+                1
+            | e ->
+                printError(e.ToString())
+                1
