4d46af06cf96781ff0e41959b77f00b2f617dd6d
Antonio Parata <aparata@gmail.com>
2019-08-05 21:31:44 +0200

    Working on updater

--- Src/Updater/Program.fs
+++ Src/Updater/Program.fs
@@ -1,7 +1,67 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿namespace Updater
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+open System
+open Argu
+open System.IO
+open ES.Fslog
+open System.Reflection
+open ES.Fslog.Loggers
+open ES.Fslog.TextFormatters
+
+module Program =
+    type CLIArguments =
+        | Verbose
+    with
+        interface IArgParserTemplate with
+            member s.Usage =
+                match s with
+                | Verbose -> "print verbose log messages."
+
+    let private printColor(msg: String, color: ConsoleColor) =
+        Console.ForegroundColor <- color
+        Console.WriteLine(msg)
+        Console.ResetColor() 
+
+    let private printError(errorMsg: String) =
+        printColor(errorMsg, ConsoleColor.Red)
+
+    let private printBanner() =
+        Console.ForegroundColor <- ConsoleColor.Cyan        
+        let banner = "-=[ Program Updater ]=-"
+        let year = if DateTime.Now.Year = 2019 then "2019" else String.Format("2019-{0}", DateTime.Now.Year)
+        let copy = String.Format("Copyright (c) {0} Enkomio {1}", year, Environment.NewLine)
+        Console.WriteLine(banner)
+        Console.WriteLine(copy)
+        Console.ResetColor()
+        
+    let private printUsage(body: String) =
+        Console.WriteLine(body)
+
+    let private configureLogProvider(isVerbose: Boolean) =
+        let path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+        let logProvider = new LogProvider()    
+        let logLevel = if isVerbose then LogLevel.Verbose else LogLevel.Informational
+        logProvider.AddLogger(new ConsoleLogger(logLevel, new ConsoleLogFormatter()))
+        logProvider.AddLogger(new FileLogger(logLevel, Path.Combine(path, "updater-client.log")))
+        logProvider :> ILogProvider  
+
+    [<EntryPoint>]
+    let main argv = 
+        printBanner()
+        
+        let parser = ArgumentParser.Create<CLIArguments>()
+        try 
+            let results = parser.Parse(argv)
+            if results.IsUsageRequested then
+                printUsage(parser.PrintUsage())                
+            else                
+                let logProvider = configureLogProvider(results.Contains(<@ Verbose @>))
+                ()
+            0
+        with 
+            | :? ArguParseException ->
+                printUsage(parser.PrintUsage())   
+                1
+            | e ->
+                printError(e.ToString())
+                1
