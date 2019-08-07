749648e462d7b879aa251246a322a0ec91514a8c
Antonio Parata <aparata@gmail.com>
2019-08-07 15:20:34 +0200

    Renamed file that contains the version number

--- Src/Updater/Utility.fs
+++ Src/Updater/Utility.fs
@@ -6,7 +6,7 @@ open System.IO
 
 module Utility =
     let readCurrentVersion() =
-        let versionFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "current-version.txt")
+        let versionFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "version.txt")
         let currentVersion = ref(new Version())
         if File.Exists(versionFile) then
             Version.TryParse(File.ReadAllText(versionFile), currentVersion) |> ignore
