0d5c4086826e250200f0e4077d1e5b1233cd71cb
Antonio Parata <aparata@gmail.com>
2019-08-08 12:35:53 +0200

    Referenced external funcs

--- Src/UpdateServer/Utility.fs
+++ Src/UpdateServer/Utility.fs
@@ -3,11 +3,9 @@
 open System
 open System.Net.NetworkInformation
 open System.IO
-open System.Security.Cryptography
 open ES.Update
 
 module internal Utility =
-
     let private getMacAddresses() =
         NetworkInterface.GetAllNetworkInterfaces()
         |> Array.filter(fun ni -> 
@@ -25,31 +23,12 @@ module internal Utility =
     let encryptKey(data: Byte array) =
         let key = getMacAddresses() |> sha256Raw
         let iv = getHardDiskSerials()
-
-        // encrypt
-        use aes = new AesManaged(Key = key, IV = iv, Padding = PaddingMode.ISO10126)
-        use ms = new MemoryStream()
-        use sw = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write)
-        sw.Write(data, 0, data.Length) 
-        sw.Close()
-        ms.ToArray()
+        CryptoUtility.encrypt(data, key, iv)
 
     let private decryptKey(data: Byte array) =
         let key = getMacAddresses() |> sha256Raw
         let iv = getHardDiskSerials()
-        Utility.decrypt(data, key, iv)
-
-    let createEncryptionKeys() =        
-        use key =
-            (new ECDiffieHellmanCng(
-                KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash,
-                HashAlgorithm = CngAlgorithm.Sha256
-            )).Key
-
-        (
-            key.Export(CngKeyBlobFormat.EccPrivateBlob) |> encryptKey |> Convert.ToBase64String,
-            key.Export(CngKeyBlobFormat.EccPublicBlob) |> Convert.ToBase64String
-        )
+        CryptoUtility.decrypt(data, key, iv)
 
     let readPrivateKey(filename: String) =
         let encodedKey = Convert.FromBase64String(File.ReadAllText(filename))
