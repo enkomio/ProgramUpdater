5a7fa84888bb7f8da7f2f33526b8c0f65fc35dff
Antonio Parata <aparata@gmail.com>
2019-08-09 07:12:41 +0200

    Added class to easier C# interoperability

---
+++ Src/ES.Update/Entities.fs
@@ -0,0 +1,9 @@
+﻿namespace ES.Update
+
+open System
+
+// create this class to easier the C# interoperability
+type Result(result: Boolean) =
+    member val Success = result with get, set
+    member val Error = String.Empty with get, set
+
