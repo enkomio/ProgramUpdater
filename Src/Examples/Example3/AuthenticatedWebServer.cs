7338a37e3cf095a491275e92dc82dab63e9836f6
Antonio Parata <aparata@gmail.com>
2019-08-18 18:18:53 +0200

    Working on Example3

---
+++ Src/Examples/Example3/AuthenticatedWebServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ES.Update;
+using ES.Update.Backend;
+
+namespace Example3
+{
+    public class AuthenticatedWebServer : WebServer
+    {
+        public static Byte[] PrivateKey = null;
+        public static Byte[] PublicKey = null;
+        public static Uri BindingUri = null;
+
+        static AuthenticatedWebServer()
+        {
+            var (publicKey, privateKey) = CryptoUtility.GenerateKeys();
+            PrivateKey = privateKey;
+            PublicKey = publicKey;
+
+            var rnd = new Random();
+            BindingUri = new Uri(String.Format("http://127.0.0.1:{0}", rnd.Next(1025, 65534)));
+        }
+
+        public AuthenticatedWebServer(String workspaceDirectory) : 
+            base(BindingUri, workspaceDirectory, PrivateKey)
+        {
+
+        }
+    }
+}
--- Src/Examples/Example3/Program.cs
+++ Src/Examples/Example3/Program.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Example3
 {
     class Program
     {
+        private static AuthenticatedWebServer _server = null;
+
+        private static void RunServer(String workspaceDirectory)
+        {
+            var wait = new ManualResetEventSlim();
+            Task.Factory.StartNew(() =>
+            {
+                _server = new AuthenticatedWebServer(workspaceDirectory);
+                wait.Set();
+                _server.Start();
+            });
+            wait.Wait();
+            Thread.Sleep(2000);
+        }
+
+        private static void RunClient(String destinationDirectory)
+        {
+            var client = new Client();
+            client.Run(_server, destinationDirectory);
+        }
+
         static void Main(string[] args)
         {
+            var (workspaceDirectory, destinationDirectory) = Example2.Helpers.CreateEnvironment();
+            RunServer(workspaceDirectory);
+            RunClient(destinationDirectory);
+            _server.Stop();
         }
     }
 }
