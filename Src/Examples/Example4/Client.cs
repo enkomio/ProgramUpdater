6f0bba0970216410eb67f579720df02a0d66f64c
Antonio Parata <aparata@gmail.com>
2019-08-21 12:19:00 +0200

    Added Example4 that use an installer

---
+++ Src/Examples/Example4/Client.cs
@@ -0,0 +1,36 @@
+﻿using ES.Update;
+using Example2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example4
+{
+    public class Client
+    {
+        public void Run(Server server, String destinationDirectory)
+        {
+            var myVersion = new Version(3, 0);
+            var updater = new Updater(server.BindingUri, "MyApplication", myVersion, destinationDirectory, server.PublicKey);
+
+            var latestVersion = updater.GetLatestVersion();
+            Console.WriteLine("My version: {0}. Latest version: {1}", myVersion, latestVersion);
+
+            if (latestVersion > myVersion)
+            {
+                // start update
+                var updateResult = updater.Update(myVersion);
+                if (updateResult.Success)
+                {                    
+                    Console.WriteLine("Everything is fine the installer program is now running. After completation you should see in this directory a file named 'file8.txt' in directory 'folder'");
+                }
+                else
+                {
+                    Console.WriteLine("Error during installing updates: {0}", updateResult.Error);
+                }
+            }
+        }
+    }
+}
---
+++ Src/Examples/Example4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Example2;
+
+namespace Example4
+{
+    public static class Program
+    {
+        private static Server _server = null;
+
+        private static void RunServer(String workspaceDirectory)
+        {
+            var wait = new ManualResetEventSlim();
+            Task.Factory.StartNew(() =>
+            {
+                _server = new Server(workspaceDirectory);
+
+                // set the installer path
+                var installerPath = Path.GetDirectoryName(typeof(Installer.Program).Assembly.Location);
+                _server.WebServer.InstallerPath = installerPath;
+
+                wait.Set();
+                _server.Start();
+            });
+            wait.Wait();
+            Thread.Sleep(2000);
+        }
+
+        private static void RunClient(String destinationDirectory)
+        {
+            var client = new Client();
+            client.Run(_server, destinationDirectory);
+        }
+
+        static void Main(string[] args)
+        {
+            var (workspaceDirectory, _) = Helpers.CreateEnvironment();
+            RunServer(workspaceDirectory);
+            RunClient(Directory.GetCurrentDirectory());
+            _server.Stop();
+        }
+    }
+}
