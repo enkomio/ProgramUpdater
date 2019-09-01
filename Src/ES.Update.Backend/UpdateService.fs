8dcc20c69706ddcc027e2b9b8b62a6877f7ec0f3
Antonio Parata <aparata@gmail.com>
2019-09-01 10:09:52 +0200

    Added thread safe and check for new versions

--- Src/ES.Update.Backend/UpdateService.fs
+++ Src/ES.Update.Backend/UpdateService.fs
@@ -1,12 +1,14 @@
 ﻿namespace ES.Update.Backend
 
 open System
+open System.Timers
 open System.IO
-open System.Collections.Generic
+open System.Collections.Concurrent
 
 type UpdateService(workspaceDirectory: String, privateKey: Byte array) =
-    let _lock = new Object()    
-    let _updateManagers = new Dictionary<String, UpdateManager>()
+    let _lock = new Object() 
+    let _timer = new Timer()
+    let _updateManagers = new ConcurrentDictionary<String, UpdateManager>()
 
     let getUpdateManager(projectName: String) =
         _updateManagers.[projectName]
@@ -20,30 +22,43 @@ type UpdateService(workspaceDirectory: String, privateKey: Byte array) =
         let latestVersion = updateManager.GetLatestVersion().Value.Version.ToString()
         String.Format("{0}-{1}.zip", correctInputVersion, latestVersion)
 
+    let doUpdate() =
+        _timer.Stop()
+        lock _lock (fun _ ->
+            Directory.GetDirectories(workspaceDirectory)
+            |> Array.map(fun directory -> Path.GetFileName(directory))
+            |> Array.iter(fun projectName -> 
+                let projectDirectory = Path.Combine(workspaceDirectory, projectName)
+                Directory.CreateDirectory(projectDirectory) |> ignore
+                _updateManagers.[projectName] <- new UpdateManager(projectDirectory)
+            )
+        )        
+        _timer.Start()
+
     do
-        Directory.GetDirectories(workspaceDirectory)
-        |> Array.map(fun directory -> Path.GetFileName(directory))
-        |> Array.iter(fun projectName -> 
-            let projectDirectory = Path.Combine(workspaceDirectory, projectName)
-            Directory.CreateDirectory(projectDirectory) |> ignore
-            _updateManagers.[projectName] <- new UpdateManager(projectDirectory)
-        )
+        _timer.Elapsed.Add(fun _ -> doUpdate())
+        _timer.Interval <- TimeSpan.FromMinutes(1.).Milliseconds |> float
+        doUpdate()
         
     member this.GetAvailableVersions() =
-        _updateManagers 
-        |> Seq.toArray
-        |> Array.collect(fun kv ->
-            kv.Value.GetAvailableVersions() 
-            |> Array.map(fun version -> (kv.Key, version))
+        lock _lock (fun _ ->
+            _updateManagers 
+            |> Seq.toArray
+            |> Array.collect(fun kv ->
+                kv.Value.GetAvailableVersions() 
+                |> Array.map(fun version -> (kv.Key, version))
+            )
         )
 
     member this.GetLatestVersion(projectName: String) =
-        if _updateManagers.ContainsKey(projectName) then
-            match getUpdateManager(projectName).GetLatestVersion() with
-            | Some application -> Some <| application.Version.ToString()
-            | None -> None
-        else
-            None
+        lock _lock (fun _ ->
+            if _updateManagers.ContainsKey(projectName) then
+                match getUpdateManager(projectName).GetLatestVersion() with
+                | Some application -> Some <| application.Version.ToString()
+                | None -> None
+            else
+                None
+        )
 
     member this.IsValidProject(projectName: String) =
         _updateManagers.ContainsKey(projectName)
